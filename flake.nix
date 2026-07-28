cbba23f42e5ea7cf7ec835292fcc8c8255368500
Dom Rodriguez <shymega@shymega.org.uk>
2026-07-28 21:26:34 +0100

    chore: Uprev Hyprland

--- flake.nix
+++ flake.nix
@@ -38,7 +38,7 @@
     };
 
     hyprland = {
-      url = "github:hyprwm/hyprland/v0.56.0";
+      url = "github:hyprwm/hyprland/v0.56.1";
       inputs.nixpkgs.follows = "nixpkgs";
       inputs.systems.follows = "systems";
       inputs.aquamarine.follows = "aquamarine";
